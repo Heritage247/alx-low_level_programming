c92d60488b93a3fce5a578e5f143930266da85a6
Heritage247 <dairotolase@gmail.com>
2022-11-02 04:00:00 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string to be printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n')
+}
+
