feb8654aa3a48669e51c48a5f7f11898c9e94326
Heritage247 <dairotolase@gmail.com>
2022-11-02 04:30:31 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be returned
+ * Return: lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+}
